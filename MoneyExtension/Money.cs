055e5682645eadd2b5bdf498e38e0f4646ed6139
Leandro Arruda <leandrolevvi@hotmail.com>
2023-01-10 21:14:04 -0300

    Criada extension

---
+++ MoneyExtension/Money.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyExtension;
+public static class Money
+{
+    public static int ToCents(this double amount){
+        if (amount == 0)
+            return 0;
+        
+        var valor = amount.ToString("N2").Replace(".", "").Replace(",", "");
+        int.TryParse(valor, out var result);
+        return result;
+    }
+}
